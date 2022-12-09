71e18c1efdef1d855b4e8dd1b59408cdbaf27529
Zaidi <zaidiahsan.r@gmail.com>
2022-12-09 16:14:30 -0600

    whie styles

--- restaurant/src/container/AboutUs/AboutUs.jsx
+++ restaurant/src/container/AboutUs/AboutUs.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { images } from '../../constants';
 import './AboutUs.css';
 
+
 const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about' >
     <div className='app__aboutus-overlay flex__center'>
@@ -13,7 +14,7 @@ const AboutUs = () => (
       <div className='app__aboutus-content_about'>
         <h1 className='headtext__cormorant'>About Us</h1>
         <img src={images.spoon} alt='about_spoon' classname="spoon__img" />
-        <p classname='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, esse et ut, similique perspiciatis iusto illum repellendus hic facilis, sunt illo tempora quasi harum beatae commodi. Dolore tempore labore rerum.</p>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, esse et ut, similique perspiciatis iusto illum repellendus hic facilis, sunt illo tempora quasi harum beatae commodi. Dolore tempore labore rerum.</p>
         <button type='button' className='custom__button'>Know More</button>
       </div>
     </div>
